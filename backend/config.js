5e2a6d8aabf3fddc1b4a6fad45e3b6f336bcdb45
jvill171 <juanvillarreal64@yahoo.com>
2023-09-01 13:58:54 -0500

    Modified code to use .env variable & updated the getDatabaseUri()

--- backend/config.js
+++ backend/config.js
@@ -9,11 +9,14 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
 
+const PG_PWD = require("./.env");
+process.env.PGPASSWORD = PG_PWD;
+
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+      ? "postgresql:///jobly_test"
+      : process.env.DATABASE_URL || "postgresql:///jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
